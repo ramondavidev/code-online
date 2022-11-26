5d79b840bb18949fb33af52cc313784beaf1b85b
Ramon <ramondavi@msn.com>
2022-11-25 23:19:49 -0300

    added esbuild and unpkgPlugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,50 @@
+import * as esbuild from "esbuild-wasm";
+import { useState, useEffect, useRef } from 'react';
 import './App.css';
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 
 function App() {
+  const ref = useRef<any>();
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    });
+  };
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+    });
+
+    // console.log(result);
+
+    setCode(result.outputFiles[0].text);
+  };
 
   return (
     <div>
-      <h1>Hi</h1>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
     </div>
   );
 }
