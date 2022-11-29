63544fd6a0c74a0ac03cd95acc1ba89a1db34422
Ramon <ramondavi@msn.com>
2022-11-28 23:33:19 -0300

    plugins working with css files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import * as esbuild from "esbuild-wasm";
+import * as esbuild from 'esbuild-wasm';
 import { useState, useEffect, useRef } from 'react';
-import './App.css';
-import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
+import ReactDOM from 'react-dom';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
 
-function App() {
+const App = () => {
   const ref = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
@@ -12,7 +12,7 @@ function App() {
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: '/esbuild.wasm',
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
     });
   };
   useEffect(() => {
@@ -28,14 +28,11 @@ function App() {
       entryPoints: ['index.js'],
       bundle: true,
       write: false,
-      plugins: [
-        unpkgPathPlugin(),
-        fetchPlugin(input)
-      ],
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
       define: {
         'process.env.NODE_ENV': '"production"',
-        global: 'window'
-      }
+        global: 'window',
+      },
     });
 
     // console.log(result);
--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -25,7 +25,6 @@ export const unpkgPathPlugin = () => {
           path: `https://unpkg.com/${args.path}`,
         };
       });
-
     },
   };
 };
