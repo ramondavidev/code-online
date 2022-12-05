61c008c37cc478010cd4aec03bdf212f440fa443
Ramon <ramondavi@msn.com>
2022-12-04 23:13:47 -0300

    Displaying code editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import * as esbuild from 'esbuild-wasm';
 import { useState, useEffect, useRef } from 'react';
-import ReactDOM from 'react-dom';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
+import CodeEditor from './components/code-editor';
 
 const App = () => {
   const ref = useRef<any>();
   const iframe = useRef<any>();
   const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
 
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.57/esbuild.wasm',
     });
   };
   useEffect(() => {
@@ -63,6 +65,10 @@ const App = () => {
 
   return (
     <div>
+      <CodeEditor
+        initialValue="const a = 1;"
+        onChange={(value) => setInput(value)}
+      />
       <textarea
         value={input}
         onChange={(e) => setInput(e.target.value)}
@@ -70,6 +76,7 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
+      <pre>{code}</pre>
       <iframe title="preview" ref={iframe} sandbox="allow-scripts" srcDoc={html} />
     </div>
   );
